86fcf1b2ff48964029fc60f407044dbc92a3569b
admin <admin@DESKTOP-2CK683T>
2021-12-06 13:55:09 +0900

    equals

---
+++ MyCountry/src/a211124.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class a211124 {
+	public static void main(String[] args) {
+		String i;
+		Scanner s=new Scanner(System.in);
+		i=s.nextLine();
+		if(i.equals("Korea")==true) {
+			System.out.println("Republick Of Korea");
+		} else {
+			System.out.println("Other Country");
+		}
+	}
+}
+// 한 프로젝트 안에 첫번째 클래스 만들 때만 main 체크

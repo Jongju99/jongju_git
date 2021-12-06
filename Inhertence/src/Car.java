7a63a12cfc30340bdb54a929e930608621538732
admin <admin@DESKTOP-2CK683T>
2021-12-06 14:22:51 +0900

    상속 배열

--- Inhertence/src/Car.java
+++ Inhertence/src/Car.java
@@ -1,20 +1,37 @@
 
 public class Car {
-	Tire frontLeft = new Tire(6, "앞 왼쪽");
-	Tire frontRight = new Tire(2, "앞 오른쪽");
-	Tire backLeft = new Tire(3, "뒤 왼쪽");
-	Tire backRight = new Tire(4, "뒤 오른쪽");
+	Tire[] tires= {
+			new Tire(6, "앞 왼쪽"),
+			new Tire(2, "앞 오른쪽"),
+			new Tire(3, "뒤 왼쪽"),
+			new Tire(4, "뒤 오른쪽")
+	};
+	
+//	Tire frontLeft
+//	Tire frontRight
+//	Tire backLeft
+//	Tire backRight
+	
+	public Car() {
+//		 frontLeft = new Tire(6, "앞 왼쪽");
+//		 frontRight = new Tire(2, "앞 오른쪽");
+//		 backLeft = new Tire(3, "뒤 왼쪽");
+//		 backRight = new Tire(4, "뒤 오른쪽");
+		
+	}
 	
 	void stop() {
 		System.out.println("자동차가 멈춥니다.");
 	}
 	int run() {
 		System.out.println("자동차가 달립니다.");
-		
-		if(!frontLeft.roll()) {stop(); return 1;}
-		if(!frontRight.roll()) {stop(); return 2;}
-		if(!backLeft.roll()) {stop(); return 3;}
-		if(!backRight.roll()) {stop(); return 4;}
+		for(int i=0; i<tires.length; i++) {
+			if(!tires[i].roll()) {stop(); return i+1;}
+		}
+//		if(!frontLeft.roll()) {stop(); return 1;}
+//		if(!frontRight.roll()) {stop(); return 2;}
+//		if(!backLeft.roll()) {stop(); return 3;}
+//		if(!backRight.roll()) {stop(); return 4;}
 		return 0;
 	}
 	
--- Inhertence/src/InheritenceSample.java
+++ Inhertence/src/InheritenceSample.java
@@ -9,19 +9,19 @@ public class InheritenceSample {
 			switch(punk) {
 			case 1:
 			System.out.println("앞 왼쪽 한국타이어로 교체");
-			car.frontLeft=new HankucTire(15, "앞 왼쪽");
+			car.tires[punk-1]=new HankucTire(15, "앞 왼쪽");
 			break;
 			case 2:
 			System.out.println("앞 오른쪽 금호타이어로 교체");
-			car.frontLeft=new KumhoTire(13, "앞 오른쪽");
+			car.tires[punk-1]=new KumhoTire(13, "앞 오른쪽");
 			break;
 			case 3:
 			System.out.println("뒤 왼쪽 한국타이어로 교체");
-			car.frontLeft=new HankucTire(14, "뒤 왼쪽");
+			car.tires[punk-1]=new HankucTire(14, "뒤 왼쪽");
 			break;
 			case 4:
 			System.out.println("뒤 오른쪽 금호타이어로 교체");
-			car.frontLeft=new KumhoTire(17, "뒤 오른쪽");
+			car.tires[punk-1]=new KumhoTire(17, "뒤 오른쪽");
 			break;
 			}
 			System.out.println("----------------------------");
